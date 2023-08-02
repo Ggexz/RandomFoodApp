737ed953d8e2dcc746fd298d77e360f7f5d4f689
Selahattin Talha Aggez <202107012044@dogus.edu.tr>
2023-08-02 19:25:26 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BenimUygulamam());
+}
+
+class BenimUygulamam extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true, //AppBar yazıyı ortalar
+          title: Text(
+            'Bugün Ne Yesem?',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        body: YemekSayfasi(),
+      ),
+    );
+  }
+}
+
+class YemekSayfasi extends StatefulWidget {
+  @override
+  State<YemekSayfasi> createState() => _YemekSayfasiState();
+}
+
+class _YemekSayfasiState extends State<YemekSayfasi> {
+  int corbaNo = 1;
+  int yemekNo = 1;
+  int tatliNo = 1;
+  List<String> corbaAdlari = [
+    'Mercimek',
+    'Tarhana',
+    'Tavuksuyu',
+    'Düğün Çorbası',
+    'Yoğurtlu Çorba'
+  ];
+  List<String> yemekAdlari = [
+    'Karnıyarık',
+    'Mantı',
+    'Kuru Fasulye',
+    'İçli Köfte',
+    'Izgara Balık'
+  ];
+  List<String> tatliAdlari = [
+    'Kadayıf',
+    'Baklava',
+    'Sütlaç',
+    'Kazandibi',
+    'Dondurma'
+  ];
+  void yemekleriYenile() {
+    setState(() {
+      corbaNo = Random().nextInt(5) + 1;
+      yemekNo = Random().nextInt(5) + 1;
+      tatliNo = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      corbaNo = Random().nextInt(5) + 1;
+                      yemekNo = Random().nextInt(5) + 1;
+                      tatliNo = Random().nextInt(5) + 1;
+                    });
+                  },
+                  child: Image.asset('assets/corba_$corbaNo.jpg')),
+            ),
+          ),
+          Text(
+            corbaAdlari[corbaNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextButton(
+                  onPressed: yemekleriYenile,
+                  child: Image.asset('assets/yemek_$yemekNo.jpg')),
+            ),
+          ),
+          Text(
+            yemekAdlari[yemekNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextButton(
+                  onPressed: yemekleriYenile,
+                  child: Image.asset('assets/tatli_$tatliNo.jpg')),
+            ),
+          ),
+          Text(
+            tatliAdlari[tatliNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
